fe88a59b3699a64fdb9803653cbec9277f3b5631
fabrizio <robertogonzalesayala@gmail.com>
2026-09-21 11:02:04 -0500

    feat(ui): add PrimeVue UI Library configuration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primmevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeflex/primeflex.css';
+import 'primicons/primeicons.css';
+import ToolTip from 'primevue/ToolTip';
 
-createApp(App).mount('#app')
+
+const primeVueLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
+createApp(App)
+    .use(PrimeVue, {theme:{preset:Material}, ripple: true, license: primeVueLicenseKey})
+    .mount('#app')
