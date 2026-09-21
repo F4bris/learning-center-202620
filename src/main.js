a17552c994db986224d939d228e5adedd141e555
fabrizio <robertogonzalesayala@gmail.com>
2026-09-21 11:12:02 -0500

    feat(in-app-navigation): configure vue-router for in-app navigation configuration

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import 'primeflex/primeflex.css';
 import 'primicons/primeicons.css';
 import ToolTip from 'primevue/ToolTip';
 import i18n from "./i18n.js";
+import router from "./router.js";
 
 
 const primeVueLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 createApp(App)
     .use(PrimeVue, {theme:{preset:Material}, ripple: true, license: primeVueLicenseKey})
     .use(i18n)
+    .use(router)
     .mount('#app')
