96a5ba37e0db4bea257d312c83b9656c5eb46bbb
fabrizio <robertogonzalesayala@gmail.com>
2026-09-21 11:18:12 -0500

    chore: clean-up assets and refuse components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './app.vue'
 import PrimeVue from 'primmevue/config';
 import Material from '@primeuix/themes/material';
 import 'primeflex/primeflex.css';
 import 'primicons/primeicons.css';
-import ToolTip from 'primevue/ToolTip';
+import ToolTip from 'primevue/tooltip';
 import i18n from "./i18n.js";
 import router from "./router.js";
 import pinia from "./pinia.js";
